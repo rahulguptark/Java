bcbb9cba6c4a76645848a21cb3d1193f6252e8f4
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-07-17 06:12:09 +0000

    Output in java

---
+++ Java_Basics/JavaBasics.java
@@ -0,0 +1,19 @@
+// Boilerplate code
+public class JavaBasics{
+    public static void main(String args[]){
+
+        // Output can be written in three types
+        
+        // System.out.print("Hello world ");
+        // System.out.println("Hello world");
+        // System.out.print("Hello world\n");
+
+        // Print pattern
+        
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+    }
+}
