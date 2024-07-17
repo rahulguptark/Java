fbd57c079ea5ad2d4579af22ebe02a994402a86e
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-07-17 06:40:04 +0000

    Sum of two numbers

---
+++ Java_Basics/Sum.java
@@ -0,0 +1,19 @@
+public class Sum {
+    
+    public static void main(String[] args) {
+        
+        // code to calculate sum of two numbers
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+    }
+}
+
+// Comments in java
+
+// This is a single line comment
+
+/* This ia a 
+Multi-line 
+comment */
