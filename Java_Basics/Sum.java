eb1c3eebd6e2ae968951ee77cd91d779ee28ddb4
rahulguptark <92668708+rahulguptark@users.noreply.github.com>
2024-07-19 05:28:39 +0000

    Sum using input

--- Java_Basics/Sum.java
+++ Java_Basics/Sum.java
@@ -1,12 +1,24 @@
+import java.util.*;
+
 public class Sum {
     
     public static void main(String[] args) {
         
         // code to calculate sum of two numbers
-        int a = 10;
-        int b = 5;
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println("Sum is: " + sum);
+
+        // Sum of two numbers by taking input from the user
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
         int sum = a + b;
-        System.out.println("Sum is: " + sum);
+        System.out.println(sum);
     }
 }
 
